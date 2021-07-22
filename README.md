386ef58de01540f3e52e16a4a51a3cef8f31a2a1
Joachim Wagner <jowagner@users.noreply.github.com>
2021-07-22 12:27:53 +0100

    add basic information and license

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# absa-rationale-eval
-Evaluate rationales in the task of aspect-based sentiment analysis against manually annoated sentiment expressions
+# ABSA Rationale Evaluation
+
+We evaluate rationales in the task of aspect-based sentiment analysis (ABSA) against manually annotated sentiment expressions.
+The software in this repository
+* fine-tunes English BERT-Base in the SemEval 2016 ABSA shared task 5, subtask "Phase B"
+* applies methods for identifying a subset of the input tokens that is most influential on the sentiment prediction (a salience map, rationale or explanation)
+* evaluates the rationales against manually annotated sentiment expressions relevant for the selected abspect term
+
+
+## Data
+
+We use the sentiment expression annotation of
+[Kaljahi, Jennifer Foster (2018)](https://aclanthology.org/W18-6222/)
+available from
+https://opengogs.adaptcentre.ie/rszk/sea
+together with the
+[SemEval 2016 task 5](http://alt.qcri.org/semeval2016/task5/index.php?id=data-and-tools)
+subtask 1 data.
+
+
+## Dependencies
+
+We develop this software with CUDA 11.1 + CUDNN, PyTorch 1.9.0,
+Hugging Face transformers and pytorch-lightning in Python 3.6.13.
+* `create-venv-pytorch.sh`: how the environment was set up
+* `pip-freeze-snapshot.txt`: all version numbers, including for indirect dependencies
+
+
+## Citation
+
+Here we will provide details on the paper this software is first used in when it is accepted.
+
---
+++ create-venv-pytorch.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ -e venv-pytorch ] ; then
+    echo "Re-using existing environment"
+else
+    virtualenv -p /usr/bin/python3.6 venv-pytorch
+    echo >> venv-pytorch/bin/activate
+    echo "# Local CUDA installation" >> venv-pytorch/bin/activate
+    echo "export PATH=/usr/local/cuda-11.1/bin:\${PATH}" >> venv-pytorch/bin/activate
+    echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.1/lib64:\${LD_LIBRARY_PATH}" >> venv-pytorch/bin/activate
+    echo >> venv-pytorch/bin/activate
+    echo "# generally not needed but some samples print warnings without these:" >> venv-pytorch/bin/activate
+    echo "export CUDA_INSTALL_DIR=/usr/local/cuda-11.1" >> venv-pytorch/bin/activate
+    echo "export CUDNN_INSTALL_DIR=/usr/local/cuda-11.1" >> venv-pytorch/bin/activate
+    # TensorRT
+    # removed as MNIST sample looks for "libnvrtc.so.11.2" but we have 11.1
+    #export LD_LIBRARY_PATH=/usr/local/TensorRT/lib:${LD_LIBRARY_PATH}
+    #export TRT_LIB_DIR=/usr/local/TensorRT/lib
+fi
+
+source venv-pytorch/bin/activate
+
+echo
+echo "Installing PyTorch..."
+echo
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+for I in \
+jupyter \
+numpy \
+scipy \
+matplotlib \
+sklearn \
+pandas \
+ipython \
+cython \
+matplotlib \
+numpy \
+dyNET \
+h5py \
+transformers \
+pytorch-lightning \
+pytorch-nlp \
+tensorboard \
+; do
+    echo
+    echo "Installing $I"
+    echo
+    pip install $I
+done
+
+echo "Please activate environment and install NVIDIA apex from github"
