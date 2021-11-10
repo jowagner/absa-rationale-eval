3d317614fc1030d58ffd36f381d79d7a74b30bb2
Joachim Wagner <jwagner+grove2018@computing.dcu.ie>
2021-11-10 11:59:53 +0000

    sort .aio items into correct order

---
+++ scripts/sort-aio-by-id.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: ascii -*-
+
+# (C) 2021 Dublin City University
+# All rights reserved. This material may not be
+# reproduced, displayed, modified or distributed without the express prior
+# written permission of the copyright holder.
+
+# Author: Joachim Wagner
+
+import sys
+
+example = """
+1:0:0     All     I
+1:0:0     I       O
+1:0:0     can     I
+1:0:0     say     I
+1:0:0     is      O
+1:0:0     W-O-W   I
+1:0:0     .       O
+
+1:2:0     It      O
+"""
+
+def sort_key(sent_id):
+    fields = sent_id.split(':')
+    assert len(fields) == 3     # (review_id, sentence_index, opinion_index)
+    return (fields[0], int(fields[1]), int(fields[2]))
+
+sentences = []
+sentence = []
+current_id = None
+while True:
+    line = sys.stdin.readline()
+    if not line or line.isspace():
+        if sentence:
+            sentences.append((sort_key(current_id), sentence))
+            sentence = []
+            current_id = None
+        if not line:
+            break
+        continue
+    fields = line.split('\t')
+    if current_id:
+        assert current_id == fields[0]
+    else:
+        current_id = fields[0]
+    sentence.append(line)
+
+sentences.sort()
+for _, sentence in sentences:
+    for line in sentence:
+        sys.stdout.write(line)
+    sys.stdout.write('\n')
+
