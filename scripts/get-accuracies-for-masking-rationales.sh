aa7b6e1db8d10fe30c86fa4772ac32789ca7d208
Joachim Wagner <jowagner@users.noreply.github.com>
2021-12-14 17:58:15 +0000

    reduce manual work for collecting masking accuracies

--- scripts/get-accuracies-for-masking-rationales.sh
+++ scripts/get-accuracies-for-masking-rationales.sh
@@ -2,9 +2,9 @@
 
 for L in L25 L50 L75; do
 
-    tail -n 5 c-f*/stdout-training-with-${L}-aio.txt | scripts/masking-summary-to-tsv.py Full     R-$L >  fig2-acc-masking-rationales-comb-${L}.tsv
-    tail -n 5 c-s*/stdout-training-with-${L}-aio.txt | scripts/masking-summary-to-tsv.py R-$L     R-$L >> fig2-acc-masking-rationales-comb-${L}.tsv
-    tail -n 5 c-o*/stdout-training-with-${L}-aio.txt | scripts/masking-summary-to-tsv.py Other-$L R-$L >> fig2-acc-masking-rationales-comb-${L}.tsv
-    tail -n 5 c-a*/stdout-training-with-${L}-aio.txt | scripts/masking-summary-to-tsv.py All-$L   R-$L >> fig2-acc-masking-rationales-comb-${L}.tsv
+    tail -n 5 c-f*/stdout-training-with-${L}-aio.txt | scripts/masking-summary-to-tsv.py Full     R-$L >  table-3-acc-masking-rationales-comb-${L}.tsv
+    tail -n 5 c-s*/stdout-training-with-${L}-aio.txt | scripts/masking-summary-to-tsv.py R-$L     R-$L >> table-3-acc-masking-rationales-comb-${L}.tsv
+    tail -n 5 c-o*/stdout-training-with-${L}-aio.txt | scripts/masking-summary-to-tsv.py Other-$L R-$L >> table-3-acc-masking-rationales-comb-${L}.tsv
+    tail -n 5 c-a*/stdout-training-with-${L}-aio.txt | scripts/masking-summary-to-tsv.py All-$L   R-$L >> table-3-acc-masking-rationales-comb-${L}.tsv
 
 done
