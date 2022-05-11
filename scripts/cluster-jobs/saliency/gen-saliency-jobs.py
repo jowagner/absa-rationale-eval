c73581259f6da9e9b64961535428ca0f969ff5a8
Joachim Wagner <jowagner@users.noreply.github.com>
2022-05-11 15:39:42 +0100

    separate job for each run for better parallelisation

--- scripts/cluster-jobs/saliency/gen-saliency-jobs.py
+++ scripts/cluster-jobs/saliency/gen-saliency-jobs.py
@@ -10,11 +10,12 @@ options   = '--load-model-from best-sea.ckpt'
 
 for shortname, longname in [
     ('f', 'Full'),
-    ('s', 'SE'),
-    ('o', 'Other'),
+    #('s', 'SE'),
+    #('o', 'Other'),
     #('a', 'All'),
 ]:
     for part in (1,2,3):
-        f = open('run-saliency-c-%(shortname)s-%(part)d1-to-%(part)d3.job' %locals(), 'w')
+      for run in (1,2,3):
+        f = open('run-saliency-c-%(shortname)s-%(part)d%(run)d.job' %locals(), 'w')
         f.write(template %locals())
         f.close()
