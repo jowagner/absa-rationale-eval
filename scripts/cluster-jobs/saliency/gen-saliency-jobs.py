77cc41a1d2922c5b5803b1bbc05a154cb385da7f
Joachim Wagner <jowagner@users.noreply.github.com>
2022-05-11 15:13:03 +0100

    specify model to be used

--- scripts/cluster-jobs/saliency/gen-saliency-jobs.py
+++ scripts/cluster-jobs/saliency/gen-saliency-jobs.py
@@ -3,13 +3,16 @@
 template = open('template-saliency.job', 'r').read()
 
 outsuffix = 'morewio-xfw'
-options = '--saliencies-from saliency-tr-stdout.txt --saliencies-from saliency-stdout-wfw.txt'
+options   = '--load-model-from best-sea.ckpt'
+
+# speed-up when saliency scores are available from a previous run:
+# --saliencies-from saliency-tr-stdout.txt --saliencies-from saliency-stdout-wfw.txt'
 
 for shortname, longname in [
     ('f', 'Full'),
     ('s', 'SE'),
     ('o', 'Other'),
-    ('a', 'All'),
+    #('a', 'All'),
 ]:
     for part in (1,2,3):
         f = open('run-saliency-c-%(shortname)s-%(part)d1-to-%(part)d3.job' %locals(), 'w')
