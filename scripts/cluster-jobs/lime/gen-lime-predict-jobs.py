9f07a4276337fe22b653123ea0640c3befe07106
Joachim Wagner <jowagner@users.noreply.github.com>
2022-06-16 11:03:59 +0100

    also use titan-v GPU on cluster

--- scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
+++ scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
@@ -9,12 +9,13 @@ template = open('template-lime-predict.job', 'r').read()
 for shortname, longname in [
     ('f', 'Full'),
 ]:
-    for part in (1,2,3,4):
-      for run, gpu, batchsize in [
-          (1, 'rtx2080ti', 128),
-          (2, 'rtx6000',   256),
-          (3, 'rtx3090',   256),
-      ]:
+    for part, gpu, batchsize, more_sbatch in [
+        (1, 'rtx6000',   256, ''),
+        (2, 'rtx3090',   256, ''),
+        (3, 'titanv',     64, '#SBATCH --reservation=themea'),
+        (4, 'rtx2080ti', 128, ''),
+    ]:
+      for run in (1,2,3):
         f = open('run-lime-predict-c-%(shortname)s-%(part)d%(run)d.job' %locals(), 'w')
         f.write(template %locals())
         f.close()
