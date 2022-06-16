718ff6f4a8b13bb2bee5ee57bcc65105796acf5d
Joachim Wagner <jowagner@users.noreply.github.com>
2022-06-16 11:53:06 +0100

    remove titan-v due to cuda env issue

--- scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
+++ scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
@@ -6,16 +6,23 @@ outsuffix = 'p%d' %int(sys.argv[1])
 
 template = open('template-lime-predict.job', 'r').read()
 
+gpus = [
+    ('rtx6000',   256, ''),
+    ('rtx3090',   256, ''),
+    #('titanv',     64, '#SBATCH --reservation=themea'),  # issue with cuda env
+    ('rtx2080ti', 128, ''),
+]
+gpu_index = 0
+
 for shortname, longname in [
     ('f', 'Full'),
 ]:
-    for part, gpu, batchsize, more_sbatch in [
-        (1, 'rtx6000',   256, ''),
-        (2, 'rtx3090',   256, ''),
-        (3, 'titanv',     64, '#SBATCH --reservation=themea'),
-        (4, 'rtx2080ti', 128, ''),
-    ]:
+    for part in (1,2,3,4):
       for run in (1,2,3):
+        gpu, batchsize, more_sbatch = gpus[gpu_index]
         f = open('run-lime-predict-c-%(shortname)s-%(part)d%(run)d.job' %locals(), 'w')
         f.write(template %locals())
         f.close()
+        gpu_index += 1
+        if gpu_index == len(gpus):
+            gpu_index = 0
