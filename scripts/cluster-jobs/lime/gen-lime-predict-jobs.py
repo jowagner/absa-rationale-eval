5cb8cb045196fcdaf6f905a9de7dd3865a9c1d9d
Joachim Wagner <jowagner@users.noreply.github.com>
2022-06-16 12:36:44 +0100

    also set deadline for job

--- scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
+++ scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
@@ -7,10 +7,10 @@ outsuffix = 'p%d' %int(sys.argv[1])
 template = open('template-lime-predict.job', 'r').read()
 
 gpus = [
-    ('rtx6000',   256, 88.5, '#SBATCH -t 19:30:00'),
-    ('rtx3090',   256, 77.5, '#SBATCH -t 21:30:00'),
-    #('titanv',     64, 77.5, '#SBATCH --reservation=themea'),  # issue with cuda env
-    ('rtx2080ti', 128, 77.5, '#SBATCH -t 21:30:00'),
+    ('rtx6000',   256, 88.5, 18.0, '#SBATCH -t 19:40:00'),
+    ('rtx3090',   256, 77.5, 20.0, '#SBATCH -t 21:50:00'),
+    #('titanv',     64, 77.5, 40.0, '#SBATCH --reservation=themea'),  # issue with cuda env
+    ('rtx2080ti', 128, 77.5, 20.0, '#SBATCH -t 21:50:00'),
 ]
 gpu_index = 0
 
@@ -19,7 +19,7 @@ for shortname, longname in [
 ]:
     for part in (1,2,3,4):
       for run in (1,2,3):
-        gpu, batchsize, speed, more_sbatch = gpus[gpu_index]
+        gpu, batchsize, speed, hours, more_sbatch = gpus[gpu_index]
         f = open('run-lime-predict-c-%(shortname)s-%(part)d%(run)d.job' %locals(), 'w')
         f.write(template %locals())
         f.close()
