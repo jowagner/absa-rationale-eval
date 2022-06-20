7e8706fe47a1c5f0ef82e3b228a01bc8e8814140
Joachim Wagner <jowagner@users.noreply.github.com>
2022-06-20 15:42:25 +0100

    include worker ID in output suffix

--- scripts/get-slurm-restart-count.py
+++ scripts/get-slurm-restart-count.py
@@ -3,12 +3,27 @@
 import os
 import sys
 
+worker_id = []
+try:
+    worker_id.append(os.environ['SLURM_JOB_ID'])
+except KeyError:
+    pass
+try:
+    worker_id.append(os.environ['SLURM_JOB_NODELIST'])
+except KeyError:
+    worker_id.append(os.uname()[1])
+try:
+    worker_id.append(os.environ['SLURM_TASK_PID'])
+except KeyError:
+    worker_id.append('%d' %os.getpid())
+
 try:
     count = os.environ['SLURM_RESTART_COUNT']
 except KeyError:
     count = 0
 
 format_str = '%%0%dd\n' %int(sys.argv[1])
+worker_id.append(format_str %int(count))
 
-sys.stdout.write(format_str %int(count))
+sys.stdout.write('-'.join(worker_id))
 
