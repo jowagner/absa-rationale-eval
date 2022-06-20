ca459bb579cbe0c155e40604da675766bf916712
Joachim Wagner <jowagner@users.noreply.github.com>
2022-06-20 20:46:48 +0100

    produce predict job files for all GPUs

--- scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
+++ scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
@@ -2,25 +2,26 @@
 
 import sys
 
-outsuffix = 'p%d' %int(sys.argv[1])
+outsuffix = 'p%s' %(sys.argv[1])
 
 template = open('template-lime-predict.job', 'r').read()
 
 gpus = [
-    ('rtx6000',   256, 88.5, 18.0, '#SBATCH -t 19:40:00'),
+    ('rtx6000',   256, 80.5, 6.0, '#SBATCH -t 07:55:00'),
     ('rtx3090',   256, 77.5, 20.0, '#SBATCH -t 21:50:00'),
     #('titanv',     64, 77.5, 40.0, '#SBATCH --reservation=themea'),  # issue with cuda env
     ('rtx2080ti', 128, 77.5, 20.0, '#SBATCH -t 21:50:00'),
 ]
 gpu_index = 0
 
-for shortname, longname in [
-    ('f', 'Full'),
-]:
+shortname, longname = ('f', 'Full')
+
+for gpu, batchsize, speed, hours, more_sbatch in gpus:   # [gpu_index]
     for part in (1,2,3,4):
       for run in (1,2,3):
-        gpu, batchsize, speed, hours, more_sbatch = gpus[gpu_index]
-        f = open('run-lime-predict-c-%(shortname)s-%(part)d%(run)d.job' %locals(), 'w')
+        #if (part, run) not in [(3,1), (4,3), ]:
+        #    continue
+        f = open('run-lime-predict-%(outsuffix)s-c-%(shortname)s-%(part)d%(run)d-%(gpu)s.job' %locals(), 'w')
         f.write(template %locals())
         f.close()
         gpu_index += 1
