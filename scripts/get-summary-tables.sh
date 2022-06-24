234a94e8c5a2cfaab364237428ba10a1eed79284
Joachim Wagner <jowagner@users.noreply.github.com>
2022-06-24 16:30:41 +0100

    compile graph data for LIME fscores

--- scripts/get-summary-tables.sh
+++ scripts/get-summary-tables.sh
@@ -2,5 +2,7 @@
 
 for FOLDER in c-f-?-? ; do 
     echo $FOLDER
-    fgrep "== Final summary for ('-', 'q0', 'test', None) ==" -A 2050 $FOLDER/saliency-morewio-xfw-stdout.txt > $FOLDER/final-test-summary.txt
+    #fgrep "== Final summary for ('-', 'q0', 'test', None) ==" -A 2050 $FOLDER/saliency-morewio-xfw-stdout.txt > $FOLDER/final-test-summary-for-R-IG.txt
+    #fgrep "== Final summary for ('-', 'q0', 'test', None) ==" -A 2050 $FOLDER/saliency-onepoint-xfw-stdout.txt > $FOLDER/final-test-summary-for-R-PG.txt
+    fgrep "== Final summary for ('" -A 2050 $FOLDER/lime-N-fscores-te.txt > $FOLDER/final-test-summary-for-R-LIME-N.txt
 done
