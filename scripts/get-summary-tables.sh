22264eb31069fbd59fd05a42cb63d6b99d146047
Joachim Wagner <jowagner@users.noreply.github.com>
2022-07-04 17:21:26 +0100

    support IG, PG and LIME rationales

--- scripts/get-summary-tables.sh
+++ scripts/get-summary-tables.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 for FOLDER in c-f-?-? ; do 
-    echo $FOLDER
-    fgrep "== Final summary for ('-', 'q0', 'test', None) ==" -A 2050 $FOLDER/saliency-morewio-xfw-stdout.txt > $FOLDER/final-test-summary.txt
+    fgrep "== Final summary for ('-', 'q0', 'test', None) ==" -A 2050 $FOLDER/saliency-morewio-xfw-stdout.txt  > $FOLDER/final-test-summary-for-R-IG.txt
+    fgrep "== Final summary for ('-', 'q0', 'test', None) ==" -A 2050 $FOLDER/saliency-onepoint-xfw-stdout.txt > $FOLDER/final-test-summary-for-R-PG.txt
+    for LIMECODE in M N S X ; do
+        echo $FOLDER $GRADTYPE
+        fgrep "== Final summary for ('" -A 2050 $FOLDER/lime-${LIMECODE}-fscores-te.txt > $FOLDER/final-test-summary-for-R-LIME-${LIMECODE}.txt
+    done
 done
