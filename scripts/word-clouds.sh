55591f2b2e15213ef867a80483a47eb0d4956275
Joachim Wagner <jowagner@users.noreply.github.com>
2022-07-04 17:37:44 +0100

    word clouds for LIME rationales

--- scripts/word-clouds.sh
+++ scripts/word-clouds.sh
@@ -13,15 +13,18 @@
 
 # requirement: https://github.com/amueller/word_cloud
 
-cat c-f*/t*-*-L50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tO\tO$' | cut -f1 | LC_ALL=C sort | uniq -c | sort -n -r > top-words-for-r-is-o-and-se-is-o.tsv
-cat c-f*/t*-*-L50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tO\tI$' | cut -f1 | LC_ALL=C sort | uniq -c | sort -n -r > top-words-for-r-is-o-and-se-is-i.tsv
-cat c-f*/t*-*-L50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tI\tI$' | cut -f1 | LC_ALL=C sort | uniq -c | sort -n -r > top-words-for-r-is-i-and-se-is-i.tsv
-cat c-f*/t*-*-L50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tI\tO$' | cut -f1 | LC_ALL=C sort | uniq -c | sort -n -r > top-words-for-r-is-i-and-se-is-o.tsv
+# L=IG, P=PG, M/N/S/X=LIME
+PREFIX=$1
 
-cat c-f*/t*-*-L50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tI\tO$' | cut -f1 > top-text-for-r-is-i-and-se-is-o.txt
-cat c-f*/t*-*-L50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tI\tI$' | cut -f1 > top-text-for-r-is-i-and-se-is-i.txt
-cat c-f*/t*-*-L50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tO\tI$' | cut -f1 > top-text-for-r-is-o-and-se-is-i.txt
-cat c-f*/t*-*-L50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tO\tO$' | cut -f1 > top-text-for-r-is-o-and-se-is-o.txt
+cat c-f*/t*-*-${PREFIX}50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tO\tO$' | cut -f1 | LC_ALL=C sort | uniq -c | sort -n -r > top-words-for-r-is-o-and-se-is-o.tsv
+cat c-f*/t*-*-${PREFIX}50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tO\tI$' | cut -f1 | LC_ALL=C sort | uniq -c | sort -n -r > top-words-for-r-is-o-and-se-is-i.tsv
+cat c-f*/t*-*-${PREFIX}50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tI\tI$' | cut -f1 | LC_ALL=C sort | uniq -c | sort -n -r > top-words-for-r-is-i-and-se-is-i.tsv
+cat c-f*/t*-*-${PREFIX}50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tI\tO$' | cut -f1 | LC_ALL=C sort | uniq -c | sort -n -r > top-words-for-r-is-i-and-se-is-o.tsv
+
+cat c-f*/t*-*-${PREFIX}50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tI\tO$' | cut -f1 > top-text-for-r-is-i-and-se-is-o.txt
+cat c-f*/t*-*-${PREFIX}50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tI\tI$' | cut -f1 > top-text-for-r-is-i-and-se-is-i.txt
+cat c-f*/t*-*-${PREFIX}50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tO\tI$' | cut -f1 > top-text-for-r-is-o-and-se-is-i.txt
+cat c-f*/t*-*-${PREFIX}50-wcloud.tsv | cut -f5,6,7 | fgrep $'\t' | grep -E $'\tO\tO$' | cut -f1 > top-text-for-r-is-o-and-se-is-o.txt
 
 for R in i o ; do
     for S in i o ; do 
@@ -36,4 +39,7 @@ for R in i o ; do
 
     done
 done
+
+mkdir wc-${PREFIX}
+mv [tw]o[pr]*-for-*-and-*.[pt][nsx][gtv] wc-${PREFIX}/
 
