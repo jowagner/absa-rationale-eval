f7812b8b89e5fa34632f58c38eacd4611bf2a2cc
Joachim Wagner <jowagner@users.noreply.github.com>
2022-07-05 22:29:02 +0100

    method 4 from wikipedia; more annotation and tuning

--- scripts/quartiles.py
+++ scripts/quartiles.py
@@ -76,14 +76,53 @@ def m_q(values, median):
     q3 = values[3*(k+1)]
     return q1, q3
 
+def helper_e_q(x, n, p):
+    k = int(p*(n+1))
+    alpha = p*(n+1) - k
+    return x[k-1] + alpha*(x[k] - x[k-1])
+
+def e_q(values, median):
+    # method 4 of https://en.wikipedia.org/wiki/Quartile
+    values = sorted(values)
+    n = len(values)
+    q1 = helper_e_q(values, n, 0.25)
+    q3 = helper_e_q(values, n, 0.75)
+    return q1, q3
+
+def helper_a_q(values, include_median = False):
+    n = len(values)
+    if n % 2 == 0 and include_median:
+        values = values[:]
+        values.append(get_median(values))
+        n += 1
+    if n % 2 == 0:
+        return r_q(values, None)
+    else:
+        values = sorted(values)
+        k = n // 4
+        if n % 4 == 1:
+            q1 = 0.25 * values[k-1] + 0.75 * values[k]
+            q3 = 0.75 * values[3*k] + 0.25 * values[3*k+1]
+        elif n % 4 == 3:
+            q1 = 0.75 * values[k] + 0.25 * values[k+1]
+            q3 = 0.25 * values[3*k+1] + 0.75 * values[3*k+2]
+        else:
+            raise ValueError
+        return q1, q3
+
+def a_q(values, median):
+    return helper_a_q(values, False)
+
+def b_q(values, median):
+    return helper_a_q(values, True)
+
 def get_synthetic_population(size, interval = 100):
     population = []
     for _ in range(size):
         values = []
-        for _ in range(8):
-            values.append(random.random())
-            values.append(random.random()**2)
-        population.append(1000+int(interval*sum(values)/16.0))
+        for _ in range(5):
+            values.append(random.random()**8)
+        population.append(1000+int(interval*sum(values)/5.0))
     return population
 
     population = []
@@ -95,6 +134,17 @@ def get_synthetic_population(size, interval = 100):
             values.append(random.random()**2)  # introducing some skew to the left
         population.append(int(200.0*sum(values)))
     return population
+
+def get_absa_accuracies():
+    f = open('data/preliminary_test_scores.txt', 'rt')
+    retval = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        retval.append(float(line.split()[0]))
+    f.close()
+    return retval
 
 def get_interpolated_range(size):
     values = []
@@ -122,8 +172,20 @@ def print_sample_with_annotation(values, annotation):
     add_annotation(annotation, r_q1, 'R-Q1')
     add_annotation(annotation, r_q3, 'R-Q3')
     m_q1, m_q3 = m_q(values, median)
-    add_annotation(annotation, m_q1, 'J-Q1')
-    add_annotation(annotation, m_q3, 'J-Q3')
+    add_annotation(annotation, m_q1, 'M-Q1')
+    add_annotation(annotation, m_q3, 'M-Q3')
+    i_q1, i_q3 = i_q(values, median)
+    add_annotation(annotation, i_q1, 'I-Q1')
+    add_annotation(annotation, i_q3, 'I-Q3')
+    a_q1, a_q3 = a_q(values, median)
+    add_annotation(annotation, a_q1, 'A-Q1')
+    add_annotation(annotation, a_q3, 'A-Q3')
+    b_q1, b_q3 = b_q(values, median)
+    add_annotation(annotation, b_q1, 'B-Q1')
+    add_annotation(annotation, b_q3, 'B-Q3')
+    e_q1, e_q3 = e_q(values, median)
+    add_annotation(annotation, e_q1, 'E-Q1')
+    add_annotation(annotation, e_q3, 'E-Q3')
     # print table
     for x in sorted(list(annotation.keys())):
         row = []
@@ -145,14 +207,20 @@ def main():
     n = int(sys.argv[1])  # sample size
     sys.stdout.write('n = %d\n' %n)
 
-    sys.stdout.write('\n== Evenly spaces values ==\n\n')
+    sys.stdout.write('\n== Evenly spaced values (integers) ==\n\n')
     sample = get_interpolated_range(n)
     annotation = {}
     annotate_values(sample, annotation)
     print_sample_with_annotation(sample, annotation)
 
-    sys.stdout.write('\n== A skewed distribution ==\n\n')
+    sys.stdout.write('\n== A skewed distribution (integers) ==\n\n')
     sample = get_synthetic_population(n)
+    annotation = {}
+    annotate_values(sample, annotation)
+    print_sample_with_annotation(sample, annotation)
+
+    sys.stdout.write('\n== Classifier accuracies ==\n\n')
+    sample = random.choices(get_absa_accuracies(), k = n)
     annotation = {}
     annotate_values(sample, annotation)
     print_sample_with_annotation(sample, annotation)
@@ -169,12 +237,16 @@ def main():
     win_r = 0
     win_m = 0
     win_i = 0
+    win_a = 0
+    win_b = 0
+    win_e = 0
     pop_unique_min = None
     pop_unique_max = None
     for _ in range(repetitions):
         if verbose:
             print('\n=== Repetition ==\n\n')
-        population = get_synthetic_population(4005, int(sys.argv[2]))  # 5 + 4k with k = 1000
+        #population = get_synthetic_population(4005, int(sys.argv[2]))  # 5 + 4k with k = 1000
+        population = get_absa_accuracies()
         unique_size = len(set(population))
         if pop_unique_min is None or unique_size < pop_unique_min:
             pop_unique_min = unique_size
@@ -185,10 +257,21 @@ def main():
         t_r_q1, t_r_q3 = r_q(population, median)
         t_m_q1, t_m_q3 = m_q(population, median)
         t_i_q1, t_i_q3 = i_q(population, median)
+        t_a_q1, t_a_q3 = a_q(population, median)
+        t_b_q1, t_b_q3 = b_q(population, median)
+        t_e_q1, t_e_q3 = e_q(population, median)
+        t_q1 = sum([t_r_q1, t_m_q1, t_i_q1, t_a_q1, t_b_q1, t_e_q1]) / 6.0
+        t_q3 = sum([t_r_q3, t_m_q3, t_i_q3, t_a_q3, t_b_q3, t_e_q3]) / 6.0
         if verbose:
-            sys.stdout.write('     \t R  \t M  \t I  \t span\n')
-            sys.stdout.write('T(Q1)\t%.2f\t%.2f\t%.2f\t%.2f\n' %(t_r_q1, t_m_q1, t_i_q1, max(t_r_q1, t_m_q1, t_i_q1) - min(t_r_q1, t_m_q1, t_a_q1)))
-            sys.stdout.write('T(Q3)\t%.2f\t%.2f\t%.2f\t%.2f\n' %(t_r_q3, t_m_q3, t_i_q3, max(t_r_q3, t_m_q3, t_i_q3) - min(t_r_q3, t_m_q3, t_a_q3)))
+            sys.stdout.write('     \t R  \t M  \t I  \t A  \t B  \t E  \t span\n')
+            sys.stdout.write('T(Q1)\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n' %(
+                t_r_q1, t_m_q1, t_i_q1, t_a_q1, t_b_q1, t_e_q1,
+                max(t_r_q1, t_m_q1, t_i_q1, t_a_q1, t_b_q1, t_e_q1) - min(t_r_q1, t_m_q1, t_a_q1, t_a_q1, t_b_q1, t_e_q1)
+            ))
+            sys.stdout.write('T(Q3)\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n' %(
+                t_r_q3, t_m_q3, t_i_q3, t_a_q3, t_b_q3, t_e_q3,
+                max(t_r_q3, t_m_q3, t_i_q3, t_a_q3, t_b_q3, t_e_q3) - min(t_r_q3, t_m_q3, t_a_q3, t_a_q3, t_b_q3, t_e_q3)
+            ))
             sys.stdout.write('\n')
             sys.stdout.write('pop range: %d to %d\n' %(min(population), max(population)))
             sys.stdout.write('\n')
@@ -199,6 +282,12 @@ def main():
         sum_m_q3 = 0.0
         sum_i_q1 = 0.0
         sum_i_q3 = 0.0
+        sum_a_q1 = 0.0
+        sum_a_q3 = 0.0
+        sum_b_q1 = 0.0
+        sum_b_q3 = 0.0
+        sum_e_q1 = 0.0
+        sum_e_q3 = 0.0
         n_samples = 1024
         for _ in range(n_samples):
             values = random.choices(population, k = n)
@@ -206,44 +295,83 @@ def main():
             r_q1, r_q3 = r_q(values, median)
             m_q1, m_q3 = m_q(values, median)
             i_q1, i_q3 = i_q(values, median)
+            a_q1, a_q3 = a_q(values, median)
+            b_q1, b_q3 = b_q(values, median)
+            e_q1, e_q3 = e_q(values, median)
             sum_r_q1 += r_q1
             sum_r_q3 += r_q3
             sum_m_q1 += m_q1
             sum_m_q3 += m_q3
             sum_i_q1 += i_q1
             sum_i_q3 += i_q3
+            sum_a_q1 += a_q1
+            sum_a_q3 += a_q3
+            sum_b_q1 += b_q1
+            sum_b_q3 += b_q3
+            sum_e_q1 += e_q1
+            sum_e_q3 += e_q3
         avg_r_q1 = sum_r_q1 / float(n_samples)
         avg_r_q3 = sum_r_q3 / float(n_samples)
         avg_m_q1 = sum_m_q1 / float(n_samples)
         avg_m_q3 = sum_m_q3 / float(n_samples)
         avg_i_q1 = sum_i_q1 / float(n_samples)
         avg_i_q3 = sum_i_q3 / float(n_samples)
+        avg_a_q1 = sum_a_q1 / float(n_samples)
+        avg_a_q3 = sum_a_q3 / float(n_samples)
+        avg_b_q1 = sum_b_q1 / float(n_samples)
+        avg_b_q3 = sum_b_q3 / float(n_samples)
+        avg_e_q1 = sum_e_q1 / float(n_samples)
+        avg_e_q3 = sum_e_q3 / float(n_samples)
         if verbose:
-            sys.stdout.write('     \t R  \t error \t M  \t error \t I  \t error \twinner\n')
-        for q, t_r_q, avg_r_q, t_m_q, avg_m_q, t_i_q, avg_i_q in [
-            (1, t_r_q1, avg_r_q1, t_m_q1, avg_m_q1, t_i_q1, avg_i_q1),
-            (3, t_r_q3, avg_r_q3, t_m_q3, avg_m_q3, t_i_q3, avg_i_q3),
+            sys.stdout.write('     \t R  \t error \t M  \t error \t I  \t error \t A  \t error \t B  \t error \t E  \t error \t winner\n')
+        for q, t_q, t_r_q, avg_r_q, t_m_q, avg_m_q, t_i_q, avg_i_q, t_a_q, avg_a_q, t_b_q, avg_b_q, t_e_q, avg_e_q in [
+            (1, t_q1, t_r_q1, avg_r_q1, t_m_q1, avg_m_q1, t_i_q1, avg_i_q1, t_a_q1, avg_a_q1, t_b_q1, avg_b_q1, t_e_q1, avg_e_q1),
+            (3, t_q3, t_r_q3, avg_r_q3, t_m_q3, avg_m_q3, t_i_q3, avg_i_q3, t_a_q3, avg_a_q3, t_b_q1, avg_b_q3, t_e_q3, avg_e_q3),
         ]:
             error_and_tag = [
-                (abs(t_r_q - avg_r_q), random.random(), 'R'),
-                (abs(t_m_q - avg_m_q), random.random(), 'M'),
-                (abs(t_i_q - avg_i_q), random.random(), 'I'),
+                (abs(t_q - avg_r_q), 'R'),
+                (abs(t_q - avg_m_q), 'M'),
+                (abs(t_q - avg_i_q), 'I'),
+                (abs(t_q - avg_a_q), 'A'),
+                (abs(t_q - avg_b_q), 'B'),
+                (abs(t_q - avg_e_q), 'E'),
             ]
-            winner = sorted(error_and_tag)[0][-1]
+            error_and_tag.sort()
+            best_error, winner = error_and_tag[0]
+            # tie detection
+            tie_index = 1
+            n_winners = 1.0
+            while tie_index < len(error_and_tag):
+                error = error_and_tag[tie_index][0]
+                if best_error < error:
+                    break
+                # found another member of the tie
+                winner = winner + '+' + error_and_tag[tie_index][1]
+                tie_index += 1
+                n_winners += 1.0
             if verbose:
-                sys.stdout.write('E(Q%d)\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%s\n' %(
+                sys.stdout.write('E(Q%d)\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%s\n' %(
                     q,
-                    avg_r_q, abs(t_r_q - avg_r_q),
-                    avg_m_q, abs(t_m_q - avg_m_q),
-                    avg_i_q, abs(t_i_q - avg_i_q),
+                    avg_r_q, abs(t_q - avg_r_q),
+                    avg_m_q, abs(t_q - avg_m_q),
+                    avg_i_q, abs(t_q - avg_i_q),
+                    avg_a_q, abs(t_q - avg_a_q),
+                    avg_b_q, abs(t_q - avg_b_q),
+                    avg_e_q, abs(t_q - avg_e_q),
                     winner,
                 ))
-            if winner == 'R':
-                win_r += 1
-            elif winner == 'M':
-                win_m += 1
-            elif winner == 'I':
-                win_i += 1
+            if 'R' in winner:
+                win_r += 1.0 #/ n_winners
+            elif 'M' in winner:
+                win_m += 1.0 #/ n_winners
+            elif 'I' in winner:
+                win_i += 1.0 #/ n_winners
+            elif 'A' in winner:
+                win_a += 1.0 #/ n_winners
+            elif 'B' in winner:
+                win_b += 1.0 #/ n_winners
+            elif 'E' in winner:
+                win_e += 1.0 #/ n_winners
             else:
                 raise ValueError
     
@@ -251,9 +379,12 @@ def main():
             sys.stdout.write('\n')
     
     sys.stdout.write('unique size range %d - %d\n' %(pop_unique_min, pop_unique_max))
-    sys.stdout.write('wins for R: %d\n' %win_r)
-    sys.stdout.write('wins for M: %d\n' %win_m)
-    sys.stdout.write('wins for I: %d\n' %win_i)
+    sys.stdout.write('wins for R: %.1f\n' %win_r)
+    sys.stdout.write('wins for M: %.1f\n' %win_m)
+    sys.stdout.write('wins for I: %.1f\n' %win_i)
+    sys.stdout.write('wins for A: %.1f\n' %win_a)
+    sys.stdout.write('wins for B: %.1f\n' %win_b)
+    sys.stdout.write('wins for E: %.1f\n' %win_e)
 
 if __name__ == "__main__":
     main()
