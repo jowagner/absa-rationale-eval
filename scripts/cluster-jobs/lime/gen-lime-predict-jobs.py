be2ec30f7c51cde8723e82421ff00626cb794736
Joachim Wagner <jowagner@users.noreply.github.com>
2022-07-09 10:34:19 +0100

    letter code for GPU used

--- scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
+++ scripts/cluster-jobs/lime/gen-lime-predict-jobs.py
@@ -7,18 +7,18 @@ outsuffix = 'p%s' %(sys.argv[1])
 template = open('template-lime-predict.job', 'r').read()
 
 gpus = [
-    ('rtx6000',   256, 80.5, 6.0, '#SBATCH -t 07:55:00'),
-    ('rtx3090',   256, 77.5, 20.0, '#SBATCH -t 21:50:00'),
-    #('titanv',     64, 77.5, 40.0, '#SBATCH --reservation=themea'),  # issue with cuda env
-    ('rtx2080ti', 128, 77.5, 20.0, '#SBATCH -t 21:50:00'),
+    ('rtx6000',   'q', 256, 80.5, 6.0, '#SBATCH -t 07:55:00'),
+    ('rtx3090',   'n', 256, 77.5, 20.0, '#SBATCH -t 21:50:00'),
+    #('titanv',    't',  64, 77.5, 40.0, '#SBATCH --reservation=themea'),  # issue with cuda env
+    ('rtx2080ti', 'r', 128, 77.5, 20.0, '#SBATCH -t 21:50:00'),
 ]
 gpu_index = 0
 
 shortname, longname = ('f', 'Full')
 
-for gpu, batchsize, speed, hours, more_sbatch in gpus:   # [gpu_index]
+for gpu, gpu_code, batchsize, speed, hours, more_sbatch in gpus:   # [gpu_index]
     # TODO: add option to add --requeue and --qos=preempt
-    gpu_first_letter = gpu[0]
+    gpu_first_letter = gpu_code
     for part in (1,2,3,4):
       for run in (1,2,3):
         #if (part, run) not in [(3,1), (4,3), ]:
