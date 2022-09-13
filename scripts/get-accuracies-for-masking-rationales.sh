1ef47ee5aba77f82b6f32b5b7bc4ed9a090f9bf2
Joachim Wagner <jowagner@users.noreply.github.com>
2022-09-13 11:08:29 +0100

    improve naming of helper script

--- scripts/get-accuracies-for-masking-rationales.sh
+++ scripts/get-accuracies-for-masking-rationales.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#tail -n 6 c-[fnsoa]-?-?/stdout-training-with-*-aio.txt | scripts/masking-summary-to-tsv.py --domains > table-3-acc-masking-rationales-comb.tsv
-tail -n 6 c-[fnsoa]-?-?/stdout-training-with-*-aio.txt | scripts/masking-summary-to-tsv.py  > table-3-acc-masking-rationales-comb.tsv
+#tail -n 6 c-[fnsoa]-?-?/stdout-training-with-*-aio.txt | scripts/masking-summary-to-tables.py --domains > table-3-acc-masking-rationales-comb.tsv
+tail -n 6 c-[fnsoa]-?-?/stdout-training-with-*-aio.txt | scripts/masking-summary-to-tables.py  > table-3-acc-masking-rationales-comb.tsv
  
